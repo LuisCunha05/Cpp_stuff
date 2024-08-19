4d3c717e1ba8013bf99238ff21f6811fbac930a0
LuisCunha05 <luisf.cunha05@gmail.com>
2024-08-19 18:35:57 -0300

    first program
    cout operator overloading example

---
+++ first.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main(void) {
+  int i;
+  float x;
+
+  i = 10 / 4;
+  x = 10.0 / 4.0;
+  cout << i << "-  " << x << "-  "; //Operator overloading
+  cout << "It's me, your first program.";
+  return 0;
+}
