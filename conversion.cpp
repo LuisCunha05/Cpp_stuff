2f2c92bc94af80a73d8d164be6d73e7ef334e51c
LuisCunha05 <luisf.cunha05@gmail.com>
2024-08-19 21:16:29 -0300

    Update conversion.cpp
    added some commented examples

--- conversion.cpp
+++ conversion.cpp
@@ -10,8 +10,8 @@ int main(void){
     i = f;
     cout << f << i; */
 
-    int i, j, k;
-    float x, y, z;
+   /*  int i, j, k;
+    float x, y, z; */
 
     /* i = 10; 
     j = 12;
@@ -20,14 +20,18 @@ int main(void){
     y = 0.5;
     z = x * y; */
 
-    i = 10; j = 5;
+    /* i = 10; j = 5;
     k = i / j;
     x = 1.0; y = 2.0;
     z = x / y;
 
     cout << k << '\n';
     printf("k: %d\n", k);
-    cout << z;
+    cout << z; */
+
+    int byte = 255;
+    cout << hex << byte << "  ";
+    cout << byte << dec << byte;
 
     return 0;
 }
