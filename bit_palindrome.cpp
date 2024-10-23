27457e68910f6a45d80d238513a179a698581c65
LuisCunha05 <luisf.cunha05@gmail.com>
2024-10-23 18:21:02 -0300

    consegui palindrome😎😎

--- bit_palindrome.cpp
+++ bit_palindrome.cpp
@@ -12,24 +12,41 @@ int main(){
     const int BITS_TO_CHECK = 15;
     unsigned short int checker = 1 << BITS_TO_CHECK;
     bool palindrome = true;
+    int counter = 0;
+    int bit_left, bit_right = 0;
+    int left = 16;
 
-    //cout << (1 << 0) << '\n';
 
-    for (int i = 0; i < BITS_TO_CHECK / 2; i++){
-        int left_c = num & checker;
-        int left = left_c >> (BITS_TO_CHECK - i);
-        int right_c = num & (1 << i);
-        int right = right_c >> i;
+    while(true){
+        bit_left = num & checker;
 
-        if(DEBUG){
-            cout << "I: " << i << ", Checker: " << checker << '\n';
-            cout << "l_c: " << left_c << ", l: " << left << ", r_c: " << right_c << ", R: " << right << '\n';
+        if(!bit_left){
+            counter++;
+            checker >>= 1;
+            continue;
         }
 
-        if(left != right){
+        left -= counter;
+        break;
+    }
+
+    int interation = left / 2;
+    for (int i = 0; i < interation; i++){
+        if(i != 0){
+            bit_left = num & checker;
+        }
+        bit_right = num & (1 << i);
+        bit_right <<= (left - i - 1);
+
+        /* if(DEBUG){
+            cout << "Counter: " << counter << ", Checker: " << checker << ", bit_left: " << bit_left << ", right: " << bit_right << ", left: " << left << '\n';
+        } */
+
+        if(bit_left != bit_right){
             palindrome = false;
             break;
         }
+        left--;
         checker >>= 1;
     }
 
