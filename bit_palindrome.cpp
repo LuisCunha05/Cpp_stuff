089cd82c607acd2ad2a12b47f8bc4269af86b213
LuisCunha05 <luisf.cunha05@gmail.com>
2024-10-24 20:53:03 -0300

    terminado palindrome bit 😜😜😜

--- bit_palindrome.cpp
+++ bit_palindrome.cpp
@@ -9,48 +9,18 @@ int main(){
     cin >> num;
 
     const bool DEBUG = true;
-    const int BITS_TO_CHECK = 15;
-    unsigned short int checker = 1 << BITS_TO_CHECK;
-    bool palindrome = true;
-    int counter = 0;
-    int bit_left, bit_right = 0;
-    int left = 16;
+    unsigned short int rev = 0;
 
 
-    while(true){
-        bit_left = num & checker;
+    for(int i = 0; i < 16; i++){
+        int bit = num & (1 << i);
 
-        if(!bit_left){
-            counter++;
-            checker >>= 1;
-            continue;
-        }
-
-        left -= counter;
-        break;
+		if(bit)
+			rev |= (1 << (15-i));
     }
 
-    int interation = left / 2;
-    for (int i = 0; i < interation; i++){
-        if(i != 0){
-            bit_left = num & checker;
-        }
-        bit_right = num & (1 << i);
-        bit_right <<= (left - i - 1);
 
-        /* if(DEBUG){
-            cout << "Counter: " << counter << ", Checker: " << checker << ", bit_left: " << bit_left << ", right: " << bit_right << ", left: " << left << '\n';
-        } */
-
-        if(bit_left != bit_right){
-            palindrome = false;
-            break;
-        }
-        left--;
-        checker >>= 1;
-    }
-
-    if(palindrome)
+    if(rev == num)
         cout << "Yes" << endl;
     else
         cout << "No" << endl;
