29e85f97d5e35ff8d3fc6ce0db0ab6ac1798831d
LuisCunha05 <luisf.cunha05@gmail.com>
2024-10-30 23:42:19 -0300

    added more function stuff

---
+++ prime.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool isPrime(int num) {
+    if(num & 1){//Fast Odd checking
+        for(int i = 3; i * i <= num; i += 2){
+            if(!(num % i))
+                return false;
+        }
+        return true;
+    }
+
+    if(num == 2)
+        return true;
+    return false;
+}
+
+int main(void) {
+	for(int i = 0; i <= 21; i++)
+		if(isPrime(i))
+			cout << i << " ";
+	cout << endl;
+	return 0;
+}
